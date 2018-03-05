7468e0f28a35a07ca97ec8acf433cfd9bf0272cd
pradeeshsuganthan <pradeeshsuganthan@gmail.com>
2018-03-04 23:27:11 -0800

    added multiple img viz + saving to file

---
+++ video.py
@@ -0,0 +1,94 @@
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+
+import coco
+import utils
+import model as modellib
+import visualize_vid
+import imageio
+
+# Root directory of the project
+ROOT_DIR = os.getcwd()
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+# Directory of images to run detection on
+VIDEO_DIR = os.path.join(ROOT_DIR, "videos/sample_nodetect")
+
+SAVE_LOC = os.path.join(ROOT_DIR, "videos/sample_detect")
+
+SAVE_VID = os.path.join(SAVE_LOC, "video.gif")
+
+
+class InferenceConfig(coco.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+
+
+
+file_names = next(os.walk(VIDEO_DIR))[2]
+
+num = len(file_names)
+
+for file in file_names:
+  image = skimage.io.imread(os.path.join(VIDEO_DIR, file))
+
+  # Run detection
+  results = model.detect([image], verbose=1)
+
+  # Visualize results
+  r = results[0]
+  images = visualize_vid.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
+                              class_names, r['scores'])
+
+  SAVE_IMG = os.path.join(SAVE_LOC, file)
+
+  images.savefig(SAVE_IMG, bbox_inches = 'tight', pad_inches = 0)
+
+
+#create video
